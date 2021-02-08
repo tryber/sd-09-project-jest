70a4135cdf60753c484475bd8269b09069198f28
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-02-08 17:54:36 -0300

    Project: Jest | Rafael Gerônimo

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("atende", async () => {
+    expect.assertions(1);
+    await expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    expect.assertions(1);
+    await expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
