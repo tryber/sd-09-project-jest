8dd1c7782e0514f9add359f7e690ac2f5010f5b1
Maurice Alves <maurice.estevam@gmail.com>
2021-02-08 16:16:56 -0300

    Requisitos 1, 2, 3 e 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -22,6 +22,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
 
+  beforeEach(() => {
+    adventure.randomAttack();
+  })
+
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
@@ -36,5 +40,8 @@ describe('quem sobreviveu?', () => {
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+
+    console.log(`Parabéns ${adventure.specialists[0].nome}! Você sobreviveu!`);
+    
   });
 });
