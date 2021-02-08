9ab1655c4bd5dfd906503d9f73e70240962c7bf2
tiago-yoneda <xyonedax@gmail.com>
2021-02-08 15:22:32 -0300

    requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -14,11 +14,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true)
+      .then(greeting => {
+        expect(greeting).toBe('Oi!');
+      })
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false)
+      .catch(message => {
+        expect(message).toBe('Infelizmente não podemos atender...');
+      })
   });
 });
