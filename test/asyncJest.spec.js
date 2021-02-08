63ed5555278de0e854f9ce827f857717ccbe4013
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-02-08 19:23:02 -0300

    answerPhone teste

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,14 +13,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe("o retorno do telefonema", () => {
   test("atende", async () => {
     // Insira seu teste assíncrono aqui
-    const answer = await answerPhone(true);
+    await expect(answerPhone(true)).resolves.toBe('Oi!')
+
     // const answer = await answerPhone(true);
-    expect(answer).toBe('Oi!')
+    // expect(answer).toBe('Oi!')
   });
   test("ocupado", async () => {
     // Insira seu teste assíncrono aqui
+    await answerPhone(false).catch(error => error);
 
-    await expect(answerPhone(false)).rejects.toBe('Infelizmente não podemos atender...')
     // const answer = await answerPhone(false);
     // console.log(answer)
     // expect(answer.rejects).toBe('Infelizmente não podemos atender...');
