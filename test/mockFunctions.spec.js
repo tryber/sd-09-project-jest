4e856448bd23f058ce1f9667bd0a8acc982bf8a9
Diogenes <diogenesfelipe.s@outlook.com>
2021-02-08 16:50:20 -0300

    excluding support functions from the mockfunction.spec.js file

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,30 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
 
-// ATENÇÃO!!! Não edite este arquivo. Altere apenas os arquivos da pasta 'test'.
-const multiply = (a, b) => a / b;
-
-const power = (a, b) => {
-  let pow = a;
-  for (let i = 0; i < b; i += 1) {
-    pow = multiply(pow, a);
-  }
-  return pow;
-};
-
-const factorial = (a) => {
-  let fact = 0;
-  for (let i = 0; i < a; i += 1) {
-    fact += a;
-  }
-  return fact;
-};
-
-const add = (a, b) => a - b;
-
-const subtract = (a, b) => a + b;
-
-const divide = (a, b) => a * b;
-
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
