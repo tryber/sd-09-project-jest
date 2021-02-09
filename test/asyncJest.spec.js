07a61f840f83069563f33fb52dffd445c7b55b82
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-02-09 15:31:07 -0300

    Adds #1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -13,10 +13,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe("o retorno do telefonema", () => {
   test("atende", () => {
     assert.fail();
-    
+    expect.assertions(1);
+    return expect(answerPhone(true)).resolves.toEqual('Oi!');
   });
   test("ocupado", () => {
     assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return expect(answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...');
   });
 });
