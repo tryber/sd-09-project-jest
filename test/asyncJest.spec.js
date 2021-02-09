345e1f5a708df07074c11000c252008534f9e8ec
Daniela Muller <danielamuller20@gmail.com>
2021-02-09 17:55:29 -0300

    requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,13 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(true).then((answer) => {
+      expect(answer).toEqual('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch((answer) => {
+      expect(answer).toEqual('Infelizmente não podemos atender...');
+    });
   });
 });
