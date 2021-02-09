90cd1d33d814d4a8b372734cfa4223c5103dc2a3
santiago <felipesantiagolds@gmail.com>
2021-02-09 17:54:39 -0300

    Requirement 3. Create a Mock for the API return

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -23,17 +23,25 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
-
   test('verifica se o usuário é o tunico', async () => {
-    return api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
-      expect(user.name.first).toEqual('Antônio');
-      expect(user.name.last).toEqual('Britto');
-      expect(user.location.country).toEqual('Brazil');
-      expect(user.email).toEqual('tunico@bol.com.br');
-      expect(user.login.username).toEqual('tunicao123');
-      expect(user.login.password).toEqual('1234567890');
-    });
+    const tunico = {
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto', },
+      location: { country: 'Brazil', },
+      email: 'tunico@bol.com.br',
+      login: { username: 'tunico123', password: '1234567890' }
+    };
+
+    let user = await api.fetchURL();
+    user = tunico;
+    console.log(user);
+    expect(user.gender).toEqual('male');
+    expect(user.name.first).toEqual('Antônio');
+    expect(user.name.last).toEqual('Britto');
+    expect(user.location.country).toEqual('Brazil');
+    expect(user.email).toEqual('tunico@bol.com.br');
+    expect(user.login.username).toEqual('tunico123');
+    expect(user.login.password).toEqual('1234567890');
+
   });
 });
