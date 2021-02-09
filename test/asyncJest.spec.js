d2da7a127d45950d32450c6b545b01c25d43d6e5
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-02-09 16:54:06 -0300

    Lesson: mockFunctions

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,7 +12,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-
+    // resolves / rejects: https://jestjs.io/docs/pt-BR/asynchronous
     expect(answerPhone(true)).resolves.toBe('Oi!');
   });
   test("ocupado", () => {
