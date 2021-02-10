6913fa2d630ecf90e826d3a49cdd373b485f5cd0
Sabrina A <sasa2605.alves@gmail.com>
2021-02-10 12:30:14 -0300

    First requirement done

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    expect(answerPhone('true')).resolves.toBe('Oi!')
     // Insira seu teste assíncrono aqui
   });
   test("ocupado", () => {
-    assert.fail();
+    expect(answerPhone()).rejects.toBe('Infelizmente não podemos atender...')
     // Insira seu teste assíncrono aqui
   });
 });
