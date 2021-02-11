9ed241b0c7cb0f688cfe3e18dc48beb31d9c3a56
Thamirys <thamirysgoncalves.prog@gmail.com>
2021-02-11 19:22:50 -0300

    realizado requisito 01

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,15 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
+  test("atende", async () => {
     // Insira seu teste assíncrono aqui
+    return answerPhone('hi').then(answer => {
+      expect(answer).toEqual('Oi!');
+    });
   });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  test("ocupado", async () => {
+    return answerPhone('').catch(error => {
+      expect(error).toEqual('Infelizmente não podemos atender...');
+    });
   });
 });
