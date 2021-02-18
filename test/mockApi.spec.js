7db1d8bcac5c6d5af1a20864e35fccc0045e1a9e
Enio Nicael <enionicael@gmail.com>
2021-02-18 00:10:49 -0300

    Requisito 3 cria um mock para o retorno da API.

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,16 +24,74 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const apiURL = jest.spyOn(api, 'fetchURL');
 
+  const user = {
+    "gender": "male",
+    "name": {
+      "title": "Mr",
+      "first": "Antônio",
+      "last": "Britto"
+    },
+    "location": {
+      "street": {
+        "number": 2953,
+        "name": "Stovnerlia"
+      },
+      "city": "Hommelstø",
+      "state": "Nordland",
+      "country": "Brazil",
+      "postcode": "6813",
+      "coordinates": {
+        "latitude": "51.7345",
+        "longitude": "121.6569"
+      },
+      "timezone": {
+        "offset": "+2:00",
+        "description": "Kaliningrad, South Africa"
+      }
+    },
+    "email": "tunico@bol.com.br",
+    "login": {
+      "uuid": "8ce84d48-dec8-462e-b90b-882e33d9ff83",
+      "username": "tunicao123",
+      "password": "1234567890",
+      "salt": "4XIzi5nM",
+      "md5": "11dfa8b2d1369680b7d3913dc1c58184",
+      "sha1": "a9acd32d6e457d43ecf954ce746787e26f1acb6b",
+      "sha256": "4602b181943ebd2118c8e0bfcde0d8bef031fc0520ea21e68832cad25936d1ad"
+    },
+    "dob": {
+      "date": "1952-02-29T00:21:20.926Z",
+      "age": 69
+    },
+    "registered": {
+      "date": "2018-08-16T19:22:59.932Z",
+      "age": 3
+    },
+    "phone": "38599523",
+    "cell": "99382466",
+    "id": {
+      "name": "FN",
+      "value": "29025248617"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/women/23.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/women/23.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/women/23.jpg"
+    },
+    "nat": "NO"
+  };
+  
   test('verifica se o usuário é o tunico', async () => {
-    return api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
-      expect(user.name.first).toEqual('Antônio');
-      expect(user.name.last).toEqual('Britto');
-      expect(user.location.country).toEqual('Brazil');
-      expect(user.email).toEqual('tunico@bol.com.br');
-      expect(user.login.username).toEqual('tunicao123');
-      expect(user.login.password).toEqual('1234567890');
-    });
+    apiURL.mockResolvedValue(user);
+
+    expect(user.gender).toEqual('male');
+    expect(user.name.first).toEqual('Antônio');
+    expect(user.name.last).toEqual('Britto');
+    expect(user.location.country).toEqual('Brazil');
+    expect(user.email).toEqual('tunico@bol.com.br');
+    expect(user.login.username).toEqual('tunicao123');
+    expect(user.login.password).toEqual('1234567890');
   });
 });
