bf5742fffd22b57e48f1976a27799459f735d338
jorgeramires <jorgerl123@gmail.com>
2021-02-21 12:27:33 -0300

    resolvendo primeiro exercicio

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -3,4 +3,4 @@ const answerPhone = answer => new Promise(function (resolve, reject) {
   setTimeout(answer ? resolve('Oi!') : reject('Infelizmente não podemos atender...'), 3000);
 });
 module.exports = answerPhone;
-primeiro commit
+
