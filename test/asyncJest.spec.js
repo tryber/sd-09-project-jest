8e2ba4ca38d2bc7bb535ad989d3533dadd823d60
Bernardo Smith <bernardostech@gmail.com>
2021-02-21 20:45:04 -0500

    questao 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,10 +12,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe("o retorno do telefonema", () => {
   test("atende", async () => {
     assert.fail('Infelizmente não podemos atender...');
-    await expect(answerPhone(true).toEqual('Oi!'))
+    await expect((answerPhone(true)).resolves.toEqual('Oi!'))
   });
   test("ocupado", async () => {
     assert.fail('Oi!');
-    await expect(answerPhone(false).toEqual('Infelizmente não podemos atender...'))
+    await expect((answerPhone(false)).rejects.toEqual('Infelizmente não podemos atender...'))
   });
 });
