f64d85474255432cbc436555185f637081c730dc
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-02-25 18:28:30 -0300

    Implemetado Requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
-  test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
-  });
+  test("atende", () => expect(answerPhone(true)).resolves.toBe('Oi!'));
+  test("ocupado", async () => answerPhone().catch(err => err));
 });
