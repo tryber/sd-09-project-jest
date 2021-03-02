77c71c571dfebb1aed64b542829f36d651f7c4cb
Pedro Amaral <amaralph1995@gmail.com>
2021-03-02 16:54:09 -0300

    Cria o teste para a função mockApi

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,6 +24,45 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const requestValue = {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
+    location: {
+      street: { number: 9974, name: 'North Street' },
+      city: 'Navan',
+      state: 'Laois',
+      country: 'Brazil',
+      postcode: 84165,
+      coordinates: { latitude: '20.2608', longitude: '-39.2911' },
+      timezone: {
+        offset: '+11:00',
+        description: 'Magadan, Solomon Islands, New Caledonia'
+      }
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      uuid: '99ff8a7d-cd43-4e1c-a825-e53fd9a04333',
+      username: 'tunicao123',
+      password: '1234567890',
+      salt: 'X2ro6gVP',
+      md5: '1f6bd396c76c0b7fa4592316d326603f',
+      sha1: '9de4561cb6320a2e6ad4b946cb7c1e0e1b69eeec',
+      sha256: 'fbd1f591d9883c061e0491a2ada096f30b68a1ab3c56046f2c00b72d28d77c95'
+    },
+    dob: { date: '1964-04-22T22:09:11.764Z', age: 57 },
+    registered: { date: '2006-01-31T09:57:09.713Z', age: 15 },
+    phone: '061-560-3222',
+    cell: '081-986-9405',
+    id: { name: 'PPS', value: '8021882T' },
+    picture: {
+      large: 'https://randomuser.me/api/portraits/men/96.jpg',
+      medium: 'https://randomuser.me/api/portraits/med/men/96.jpg',
+      thumbnail: 'https://randomuser.me/api/portraits/thumb/men/96.jpg'
+    },
+    nat: 'IE'
+  }
+
+  api.fetchURL = jest.fn().mockResolvedValue(requestValue);
 
   test('verifica se o usuário é o tunico', async () => {
     return api.fetchURL().then((user) => {
