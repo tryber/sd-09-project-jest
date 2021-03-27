799208d6ea5837ec21ebde830a9cc010a1125b6c
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-03-27 16:31:49 -0300

    2 requirement - finished

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    answerPhone(true).then(answer => 
+      expect(answer).toEqual('Oi!'));
   });
   test("ocupado", () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    // expect(answerPhone(false)).toBe('Infelizmente não podemos atender...');
+    answerPhone(false).catch(error => 
+      expect(error).toEqual('Infelizmente não podemos atender...'));
+    });
   });
-});
+// ATENÇÃO!!! Não edite este arquivo. Altere apenas os arquivos da pasta 'test'.
